4755b2cf4b25d4dc49be1103a86541306845d4f4
Ahmed Ebrahim <eng.ahmed0302@gmail.com>
2025-07-01 04:30:11 +0300

    Test Push from local to github to server using ci/cd

--- resources/views/web/app.blade.php
+++ resources/views/web/app.blade.php
@@ -8,7 +8,6 @@
     <title>@yield('title', 'Ramco')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link rel="icon" href="{{ asset('static/logo.webp') }}">
