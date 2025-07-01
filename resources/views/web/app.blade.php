7aaaf8c286b75bf36addbdb882866cd8f3f173bb
Ahmed Ebrahim <eng.ahmed0302@gmail.com>
2025-07-01 04:17:48 +0300

    Update

--- resources/views/web/app.blade.php
+++ resources/views/web/app.blade.php
@@ -6,7 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Ramco')</title>
-    @vite(['resources/js/app.js'])
+    <!-- Bootstrap CSS من CDN -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Bootstrap JS من CDN -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link rel="icon" href="{{ asset('static/logo.webp') }}">
