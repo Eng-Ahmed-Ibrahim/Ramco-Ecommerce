966588a1af69bf339d21f554aadeb1c8e57a7c85
Ahmed Ebrahim <eng.ahmed0302@gmail.com>
2025-07-02 04:55:11 +0300

    Update app component

--- resources/views/web/app.blade.php
+++ resources/views/web/app.blade.php
@@ -11,7 +11,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 
-    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}?v={{ time() }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link rel="icon" href="{{ asset('static/logo.webp') }}">
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
