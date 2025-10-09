440cfce6ed93bd02d76c92ad093a8c72e4f7b017
Ahmed Ebrahim <eng.ahmed0302@gmail.com>
2025-10-09 23:14:08 +0300

    Fixed Bugs

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,7 +26,7 @@ class DatabaseSeeder extends Seeder
                 // CurrencySeeder::class,
                 // AdminSeeder::class,
                 PermissionSeeder::class,
-                // SettingsSeeder::class,
+                SettingsSeeder::class,
         ]);
     }
 }
